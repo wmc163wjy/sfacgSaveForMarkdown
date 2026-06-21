10779cff7289e906d8e3200e210cdb84482061e2
wmc163 <120155881+wmc163wjy@users.noreply.github.com>
2026-06-21 08:22:11 +0800

    添加图片下载配置选项

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
   // 保存方式选择：1为原有方式（单个md文件），2为按书名/卷名创建文件夹、章节名为md名称
   saveMode: 2,
   // 并发保存线程数
-  concurrentThreads: 3
+  concurrentThreads: 3,
+  // 是否下载图片：true=下载，false=不下载
+  downloadImages: true
 }
